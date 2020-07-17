6078aaa741121f46561f3325f5f4bdd9ca8862f7
Deathy <rafatorrea@gmail.com>
2020-07-17 20:12:09 +0200

    Add devTools RequestMapping users

--- src/main/java/es/rafa/cmstraining/controller/UserController.java
+++ src/main/java/es/rafa/cmstraining/controller/UserController.java
@@ -7,11 +7,13 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/users")
 public class UserController {
 
     @Autowired
@@ -24,11 +26,4 @@ public class UserController {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(userService.getAllUsers());
     }
-
-//    @GetMapping("/getAllUsers")
-//    public List<UserDTO> getAllUsers() {
-//        return userService.getAllUsers();
-//    }
-
-
 }
